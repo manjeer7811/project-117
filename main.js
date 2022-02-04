1e992218378f081c44badc110b8cdd968342e382
Manjeer Chakraborty <84231083+manjeer7811@users.noreply.github.com>
2022-02-04 13:05:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(280,280);
+canvas.center();
+background("white");
+
+}
+function clearCanvas(){
+    background("white")
+}
+function draw(){
+    
+}
